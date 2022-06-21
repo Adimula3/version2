1b031861fe8c6025af743abfe8ad75e2fa5935de
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-06-21 15:04:33 -0500

    demo 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from "./pages/header";
 import Hero from "./pages/hero";
 import Products from "./pages/products";
-import Contact from "./pages/contact";
+
 
 function App() {
   return (
